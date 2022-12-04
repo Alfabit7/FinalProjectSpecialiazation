2e3d58dd257906491556a736056d6f698f7bf9d1
Alfabit7 <tritg@yandex.ru>
2022-12-04 18:15:22 +0300

    added file block-shema and change code and readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача: 
+Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Примеры:
+[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+[“Russia”, “Denmark”, “Kazan”] → []
+*/
+
+// Очищаем консоль 
+Console.Clear();
+
+// Просим пользователея ввести  текст
+Console.Write("Введите через пробел: ");
+// Ввод сохраняем в новый массив очистив от лишних пробелов 
+string[] userInput = Console.ReadLine().Split(new char[] { }, StringSplitOptions.RemoveEmptyEntries);
+
+// sizeUserInputFiltered хранит размер нового массива
+int sizeUserInputFiltered = 0;
+
+// Считает сколько элементов  в массиве соответстует условию длины меньше 3 символов
+foreach (string el in userInput)
+{
+    if (el.Length <= 3) { sizeUserInputFiltered += 1; }
+    else { continue; }
+}
+// Создаем новый массив размером sizeUserInputFiltered
+string[] userInputFiltered = new string[sizeUserInputFiltered];
+
+// Проверяем, что новый массив не пустой
+if (sizeUserInputFiltered != 0)
+{
+    FilteredArray(userInput);
+    // Выводим новый массив на экран 
+    Console.Write($" Новый массив {String.Join("; ", userInputFiltered)};");
+
+}
+// Если массив пустой выводим сообщение на экран, что массив пуст.
+else { Console.Write(" Новый массив  пуcт []"); }
+
+
+// Функция заполняет новый массив элементами из принятого массива.
+// В новый массив войдут те элементы, у которых кол-во символов  меньше либо равна 3
+void FilteredArray(string[] arr)
+{
+    for (int i = 0, j = 0; i < userInput.Length; i++)
+    {
+        if (userInput[i].Length <= 3)
+        {
+            userInputFiltered[j] = userInput[i];
+            j++;
+        }
+        else { continue; }
+    }
+}
+
+
+
